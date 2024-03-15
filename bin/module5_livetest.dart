a4922a9f3f00e92b338685edcffcf21c70a2b17b
Riad-383 <hossainriad383@gmail.com>
2024-03-15 23:25:43 +0600

    module5livetest

---
+++ bin/module5_livetest.dart
@@ -0,0 +1,40 @@
+import 'package:module5_livetest/module5_livetest.dart' as module5_livetest;
+
+
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+
+  print("Original Fruit Details before Discount:");
+
+  for (var fruit in fruits) {
+    print("Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price'].toStringAsFixed(2)}");
+  }
+}
+
+
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discountPercentage) {
+
+  for (var fruit in fruits) {
+    fruit['price'] -= (fruit['price'] * (discountPercentage / 100));
+  }
+}
+
+
+void main() {
+
+  List<Map<String, dynamic>> fruits = [
+    {'name': 'Apple', 'color': 'Red', 'price': 2.5},
+    {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
+    {'name': 'Grapes', 'color': 'Purple', 'price': 3.0}
+  ];
+
+
+  displayFruitDetails(fruits);
+
+
+  applyPriceDiscount(fruits, 10);
+
+
+  print("\nFruit Details After Applying 10% Discount:");
+  displayFruitDetails(fruits);
+}
